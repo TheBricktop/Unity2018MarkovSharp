78a2849126376364115cfdc243377d69ec09b138
TheBricktop <3866030+TheBricktop@users.noreply.github.com>
2018-12-29 12:25:59 +0100

    Fixed dependency on valve.newtonsoft.json

--- MarkovSharpUnity/Models/NgramWrapper.cs
+++ MarkovSharpUnity/Models/NgramWrapper.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Valve.Newtonsoft.Json;
+using Newtonsoft.Json;
 
 namespace MarkovSharp.Models
 {
